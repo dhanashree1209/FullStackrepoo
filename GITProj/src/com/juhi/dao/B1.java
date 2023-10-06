8942ef5a9761099cf821f0afdf1aa2719aed5eee
dhanashree1209 <146727896+dhanashree1209@users.noreply.github.com>
2023-10-06 19:14:06 +0530

    Updation-Dev1

---
+++ GITProj/src/com/juhi/dao/B1.java
@@ -0,0 +1,9 @@
+package com.juhi.dao;
+
+public class B1 {
+	
+	public int add(int x,int y) {
+		return x+y;
+	}
+
+}
--- GITProj/src/com/juhi/dao/XYZ.java
+++ GITProj/src/com/juhi/dao/XYZ.java
@@ -1,13 +1,14 @@
 package com.juhi.dao;
 
-public class XYZ {
-
-	public void m1() {
+public class XYZ 
+{
+public void m1()
+{
 
 		System.out.println("XYZ(method) : m1 for testing");
-
-	}
-
-
-
+        
+		B1 b1 = new B1();
+		int result = b1.add(10, 20);
+		System.out.println("Result:"+result);
+}
 }
